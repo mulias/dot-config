7d30e4dc55a04e4c53f2f9ab989e1517b802ab1f
Elias Mulhall <eli.mulhall@gmail.com>
2021-08-07 13:16:53 -0400

    [nvim] Fix typo

--- nvim/.config/nvim/lua/em/config/ui.lua
+++ nvim/.config/nvim/lua/em/config/ui.lua
@@ -5,8 +5,8 @@ Picking a theme requires knowing the color pallet limits of your terminal
 emulator. Some terminals are limited to 16 or 256 colors, while 'true color'
 terminals have the full hex color range. Counterintuitive, true color terminals
 tend to set $TERM to 'xterm-256color', even though more than 256 colors are
-available. The `can_spport_true_color` test might need to be updated to include or
-exclude regularly used terminals.
+available. The `can_support_true_color` test might need to be updated to
+include or exclude regularly used terminals.
 
 Use something close to the default statusline, with the addition of indicating
 the current git branch (requires fugitive) and showing linting results
