cd082fb12e7fb7656a308096f15f7d049af8fead
Elias Mulhall <eli.mulhall@gmail.com>
2024-07-31 12:37:41 -0400

    Move many nix packages from nixpkgs config to system config

--- nixpkgs/.config/nixpkgs/config.nix
+++ nixpkgs/.config/nixpkgs/config.nix
@@ -8,52 +8,31 @@ in {
     eliasAll = pkgs.buildEnv {
       name = "elias-all";
       paths = [
-        adapta-gtk-theme
-        arc-icon-theme
-        baobab
-        bat
+        (python3.withPackages my-python-packages)
         calibre
-        catdocx
-        connman-gtk
-        direnv
-        diskus
-        fd
-        feh
-        ffmpeg
-        figlet
         galculator
         gimp
         gitAndTools.qgit
-        gnome3.totem
         gparted
         keepassxc
         libreoffice
-        links2
-        mplayer
+        megasync
         nox
-        pandoc
         pdfslicer
         pinta
-        (polybar.override { i3Support = true; pulseSupport = true; })
         qpdfview
-        racket
-        ripgrep
-        silver-searcher
-        unstable.signal-desktop
         texlive.combined.scheme-full
         texworks
-        universal-ctags
+        transmission_4-gtk
         unstable.discord
-        unstable.fzf
+        unstable.firefox
         unstable.google-chrome
         unstable.kitty
-        unstable.neovim
-        (unstable.python3.withPackages my-python-packages)
-        unstable.vlc
-        unstable.zoom-us
-        unstable.firefox
-        unzip
-        youtube-dl
+        unstable.signal-desktop
+        unstable.zed-editor
+        vscode
+        dbeaver-bin
+        git-absorb
       ];
     };
   };
