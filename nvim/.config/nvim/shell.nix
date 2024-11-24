123ca0ec6b2fab59f2d260f9d181436d8e504ec8
Elias Mulhall <eli.mulhall@gmail.com>
2024-11-24 11:08:05 -0500

    Remove unnecessary env vars for lua lsp

--- nvim/.config/nvim/shell.nix
+++ nvim/.config/nvim/shell.nix
@@ -16,8 +16,5 @@ mkShell {
   shellHook = ''
     export NVIM_NVIM_LUA_LSP=true
     export NVIM_STYLUA_LSP=true
-    export LUA_LS_ROOT_PATH=$(nix eval nixos.sumneko-lua-language-server.outPath)
-    export LUA_LS_MAIN_FILE="$LUA_LS_ROOT_PATH/extras/main.lua"
-    export LUA_LS_EXECUTABLE=lua-language-server
   '';
 }
